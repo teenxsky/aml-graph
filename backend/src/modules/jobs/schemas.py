1ab59c53f1df28b4582268d4f396e28a2ab5622c
teenxsky <teenxsky@gmail.com>
2026-05-18 01:05:37 +1000

    Add job info schema

--- backend/src/modules/jobs/schemas.py
+++ backend/src/modules/jobs/schemas.py
@@ -3,15 +3,34 @@ from datetime import datetime
 from pydantic import BaseModel
 
 __all__ = [
+    'JobCreated',
     'JobInfo',
 ]
 
-from src.modules.jobs.enums import JobStatus
+from src.modules.jobs.enums import JobStatus, UploadFormat
 
 
-class JobInfo(BaseModel):
-    """Информация о задаче обработки датасета."""
+class JobCreated(BaseModel):
+    """Информация о постановке задачи в очередь на обработку данных."""
 
     job_id: str
     status: JobStatus
     created_at: datetime
+
+
+class JobInfo(BaseModel):
+    """Информация задаче на обработку данных."""
+
+    id: str
+    status: JobStatus
+    format: UploadFormat
+    user_ip: str
+    file_path: str | None
+    column_mapping: dict | None
+    ladybug_ref: str | None
+    detector_results: dict | None
+    error_msg: str | None
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = {'from_attributes': True}
