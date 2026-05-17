54ee9795d7915099e224af262a954a86ffb59794
teenxsky <teenxsky@gmail.com>
2026-05-18 01:10:45 +1000

    Register new tasks

--- backend/src/tasks/__init__.py
+++ backend/src/tasks/__init__.py
@@ -1,4 +1,6 @@
 from src.tasks.build_graph import *  # noqa: F403
+from src.tasks.cluster_graph_task import *  # noqa: F403
 from src.tasks.detect_patterns import *  # noqa: F403
+from src.tasks.hierarchical_layout_task import *  # noqa: F403
 from src.tasks.save_graph import *  # noqa: F403
 from src.tasks.score_and_layout import *  # noqa: F403
