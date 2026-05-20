ce40531e7e7feac7a6dc978783223851e95c1256
teenxsky <teenxsky@gmail.com>
2026-05-20 16:04:35 +1000

    Add backend url

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next'
 
+const BACKEND_URL = process.env.NEXT_PUBLIC_API_BASE ?? 'http://localhost:9090'
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${BACKEND_URL}/:path*`
+      }
+    ]
+  }
 }
 
 export default nextConfig
